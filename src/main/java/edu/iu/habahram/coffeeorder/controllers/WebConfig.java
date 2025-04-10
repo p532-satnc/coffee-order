f800bc7be40239687ace59bb73f4da6115c5b852
N C, Sathya <satnc@iu.edu>
2025-04-09 23:20:23 -0400

    CORS

---
+++ src/main/java/edu/iu/habahram/coffeeorder/controllers/WebConfig.java
@@ -0,0 +1,15 @@
+package edu.iu.habahram.coffeeorder.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://p532-satnc.github.io");
+    }
+}
